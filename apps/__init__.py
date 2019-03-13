f6145f495b7108c8f8de2ece92d10871c4b99aab
PUXU <puxu@kingsoft.com>
2019-03-13 16:31:44 +0800

    first commit

---
+++ apps/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time : 19-1-25 下午4:59
+# @Author : puxu
+# @File : __init__.py.py
+# @Software: PyCharm
---
+++ extra_apps/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time : 19-1-25 下午5:00
+# @Author : puxu
+# @File : __init__.py.py
+# @Software: PyCharm
